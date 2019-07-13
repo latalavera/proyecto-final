11e4aa68623cbbbd5c8d48c139c1e2ce3476a42e
latalavera <jesusfernandez0586@gmail.com>
2019-07-13 12:45:40 +0200

    second commit

--- main.py
+++ main.py
@@ -1,38 +1,104 @@
-from flask import Flask, render_template, request, make_response, redirect, url_for
-from models import Loginform
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask import Flask, render_template, redirect, url_for, request, make_response
+from models import Loginform, Registerform, User, Mensaje, db, Messageform, get_choices
+from flask_login import login_required, logout_user, LoginManager, login_user, current_user
+from flask_bootstrap import Bootstrap
+from werkzeug.security import generate_password_hash, check_password_hash       #security features for the password
+#from flask_socketio import SocketIO, send
+
 
 app = Flask(__name__)
+db.create_all()
 app.config["SECRET_KEY"] = "1234567890"
+Bootstrap(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.query(User).get(int(user_id))
+
 
 @app.route("/")
 def index():
-    return (render_template("index.html"))
+    return render_template("index.html", current_user=current_user)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = Loginform()
-    return (render_template("login.html"))
+    if form.validate_on_submit():
+        user = db.query(User).filter_by(name=form.name.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):  #this function automatically compares the 2 passwords (user.password and form.password)
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for("dashboard"))
+        return "Invalid username or password"
+    return render_template("login.html", form=form, current_user=current_user)
 
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return (render_template("signup.html"))
+    form = Registerform()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method="sha256")   #this generates a 80 character long password
+        new_user = User(name=form.name.data, email=form.email.data, password=hashed_password, city=form.city.data)
+        db.add(new_user)
+        db.commit()
+        return redirect(url_for("login"))
+    return render_template("signup.html", form=form, current_user=current_user)
 
 
-@app.route("/dashboard")
+@app.route("/dashboard", methods=["GET", "POST"])
+@login_required
 def dashboard():
-    return (render_template("dashboard.html"))
+    users = db.query(User).all()
+    form = Messageform()
+    form.receiver_email.choices = get_choices(current_user)
+    if request.method == "GET":
+        messages = db.query(Mensaje).filter_by(read=0).order_by(Mensaje.date.desc())
+        return render_template("dashboard.html", messages=messages, form=form, current_user=current_user, users=users)
+
+    else:
+        if form.validate_on_submit():
+            msg = Mensaje(subject=form.subject.data, message=form.message.data, receiver=form.receiver_email.data,
+                          sender=current_user.email, read=False)
+            db.add(msg)
+            db.commit()
+            data = "Your message has been sent"
+            return render_template("dashboard.html", data=data, form=form, current_user=current_user, users=users,
+                                   msg=msg)
+
+
+@app.route('/messages/<msg_id>')
+@login_required
+def message_details(msg_id):
+    msg = db.query(Mensaje).get(int(msg_id))
+    msg_sender = list(db.query(Mensaje).filter_by(sender=msg.sender))
+    msg_receiver = list(db.query(Mensaje).filter_by(receiver=msg.receiver))
+
+    messages = msg_sender + msg_receiver
+
+    messages.sort(key=lambda s: s.date)
+
+    read = list(filter(lambda s: s.read, messages))
+    non_read = list(filter(lambda s: not s.read, messages))
+
+    for msg in non_read:
+        msg.read = True
+
+    return render_template("messages.html", msg_read=read, msg_non_read=non_read, current_user=current_user)
+
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
 
 
--- models.py
+++ models.py
@@ -1,16 +1,55 @@
 import os
+import datetime
 from sqla_wrapper import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, length
+from wtforms import StringField, PasswordField, BooleanField, SelectField
+from wtforms.validators import InputRequired, Email, Length
+from flask_login import UserMixin
+from wtforms.fields.html5 import EmailField
+
 
 db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite"))  # this connects to a database either on Heroku or on localhost
 
 
+class User(UserMixin, db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+    city = db.Column(db.String(20))
+
+
+class Mensaje(db.Model, UserMixin):
+    __tablename__ = "mensaje"
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String)
+    message = db.Column(db.Text)
+    receiver = db.Column(db.String, db.ForeignKey("user.email"))
+    sender = db.Column(db.String, db.ForeignKey("user.email"))
+    read = db.Column(db.Boolean, default=False)
+    date = db.Column(db.DateTime, default=datetime.datetime.now)
+
+
 class Loginform(FlaskForm):
-    name = StringField("username", validators=[InputRequired, length(min=4, max=15)])
-    password = PasswordField("password", validators=[InputRequired, length(min=8, max=80)])
-    remember = BooleanField("rememberme")
+    name = StringField("username", validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField("password", validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField("remember me")
 
 
+class Registerform(FlaskForm):
+    name = StringField("username", validators=[InputRequired(), Length(min=4, max=15)])
+    email = StringField("email", validators=[InputRequired(), Length(min=4, max=50)])
+    password = PasswordField("password", validators=[InputRequired(), Length(min=8, max=80)])
+    city = StringField("city", validators=[InputRequired(), Length(min=4, max=20)])
 
+
+def get_choices(my_user):
+    users = db.query(User).all()
+    return [(user.email, user.email) for user in users if user.email != my_user.email]
+
+
+class Messageform(FlaskForm):
+    subject = StringField("Subject", [InputRequired(), Length(min=4, max=500)])
+    message = StringField("Message", [InputRequired(), Length(min=4, max=500)])
+    receiver_email = SelectField("Receiver email", [InputRequired(), Email("Introduce a valid email address"), Length(min=4, max=50)])
